9bc609c3866f897cd06b208adc8ef3555bb53ec7
Soumojit <sanjugon2003@gmail.com>
2025-08-26 01:21:53 +0530

    required changes

--- backend/server.js
+++ backend/server.js
@@ -3,28 +3,32 @@ import dotenv from 'dotenv'
 import { connectDb } from './config/db.js'
 import productRoutes from './routes/product.route.js'
 import path from 'path'
+
 // Load environment variables
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
-
 const __dirname = path.resolve()
+
 // Middleware
 app.use(express.json())
 
-// Product routes
+// Product API routes
 app.use("/api/products", productRoutes)
 
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static(path.join(__dirname , "/frontend/dist")))
+if(process.env.NODE_ENV === "production"){
+    // Serve static files from React frontend
+    app.use(express.static(path.join(__dirname, "frontend", "dist")))
 
-    app.get("*" , (req,res) =>{
-        res.sendFile(path.resolve(__dirname , "frontend" , "dist" , "index.html"))
+    // Catch-all for React routing
+    app.use((req, res, next) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
     })
 }
 
+// Start server
 app.listen(PORT, () => {
     connectDb()
-    console.log('Server started at http://localhost:'+ PORT)
+    console.log(`Server started at http://localhost:${PORT}`)
 })
